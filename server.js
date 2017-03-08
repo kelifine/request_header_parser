f1640a15817de7e8d1cbf6ee8b0466ca6f6a8320
Keli Fine <kelfin373@gmail.com>
2017-03-08 16:16:58 +0000

    first answer

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+var object; 
+
+function requester (req) {
+     var ip = req.ip;
+     var lang = req.headers['accept-language'];
+     var soft = req.headers['user-agent'];
+     object = {
+        ipaddress: ip.substr(ip.lastIndexOf(':')+1),
+        language: lang.substr(0, lang.indexOf(',')),
+        software: soft.substr(0, soft.indexOf(')')+1)
+    };
+}
+
+app.get('', function (req,res) {
+    console.log(req.ip);
+   requester(req);
+    res.send(object);
+});
+
+app.listen(8080);
+
+
